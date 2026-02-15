42d9a2926041f8b5464bd5ee30d1a533ae8de061
p2crafts <p2blacketer@gmail.com>
2026-02-15 02:01:04 +0000

    finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,6 @@
+print ("hello world!")
+print ("my nameis Phoebe")
+print ("I like dogs")
+print ("I LOVE food!!!!")
+# print ("I'm Phoebe!")
+# Error: quotation marks mismatched
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,8 @@
+x = 5 # integer: whole numbers without a decimal
+print (x)
+
+favorite_food = "Mac & Cheese"
+print (favorite_food)
+print (x,  favorite_food)
+print ("my favorite food is, favorite food")
+y = 3.25 # float: number with a demimal point
