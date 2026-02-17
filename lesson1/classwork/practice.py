75ea10389b7a299422228ab1dc3b3264e312eae1
p2crafts <p2blacketer@gmail.com>
2026-02-17 18:26:29 +0000

    finished lesson 1 practice

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,35 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
+favorite_number = 2
+print (favorite_number)
 
 
 # Problem 2
 # Create a variable for your height in meters and print it.
+my_height = 1.2
+print (my_height)
 
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
+favorite_animal = "flamingo"
+print ("My favorite animal is " + favorite_animal)
 
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
+my_first_name = "Phoebe"
+my_last_name = "Blacketer"
+print (my_first_name) 
+print (my_last_name) 
 
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+age = "9"
+color = "blue"
+print ("My name is " + my_first_name + ", I'm " + age + " years old and my favorite color is " + color + ".")
